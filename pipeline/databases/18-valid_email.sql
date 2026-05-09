bee6221f8608e5f1a96afcb89a8c4195ec93ace9
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-10 01:42:03 +0200

    adding new query

---
+++ pipeline/databases/18-valid_email.sql
@@ -0,0 +1,16 @@
+-- Reset the valid_email flag when an email is updated
+
+DROP TRIGGER IF EXISTS reset_valid_email;
+
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END$$
+
+DELIMITER ;
