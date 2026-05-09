632ed26d4459039e27f479f461553000a1e148fc
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-09 23:35:06 +0200

    adding new query

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,10 @@
+-- This query calculates the total rating for each TV show by summing up the ratings from the tv_show_ratings table.
+-- It uses a LEFT JOIN to include all TV shows, even those that have no ratings, and COALESCE to return 0 for shows without ratings.
+-- The results are grouped by TV show ID and ordered by rating in descending order.
+
+SELECT tv_shows.title AS title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.id
+ORDER BY rating DESC;
