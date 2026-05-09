e9e9534e87b1171a16b9656667698364f15d89ec
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-10 01:30:39 +0200

    adding new query

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,14 @@
+-- Create a trigger to decrease item quantity after an order is placed
+
+DELIMITER $$
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE id = NEW.item_id;
+END$$
+
+DELIMITER ;
