c008cf6f55380363c58c251421a8a72f98bdc90d
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-10 00:28:45 +0200

    adding new table

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,3 +1,6 @@
+-- This SQL script creates a table named 'users' to store user information with a country attribute.
+-- The table includes an auto-incrementing 'id' as the primary key, a unique 'email', and a 'country' field.
+
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
