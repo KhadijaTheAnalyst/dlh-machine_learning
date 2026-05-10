543307419432853edb300beb1055b58e87af3891
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-10 20:55:49 +0200

    Task 22: List MongoDB databases using PyMongo

---
+++ pipeline/databases/22-list_databases.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""List all MongoDB databases"""
+
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+
+# List all databases
+databases = client.list_database_names()
+
+# Print each database
+for db in databases:
+    print(db)
+
+client.close()
