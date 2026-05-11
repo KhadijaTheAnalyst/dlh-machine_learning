0eb90053e7b913fa58dc8ab3e1b84d89b360e1dc
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-11 16:09:24 +0200

    Add slice me up

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python3
-"""This script demonstrates how to slice an array in Python.
-It creates an array of numbers and then slices it to obtain specific subsets of the array.
-The first slice retrieves the first two numbers, the second slice retrieves the last five numbers,
-and the third slice retrieves the 2nd through 6th numbers of the array. Finally,
-it prints the results.
-"""
-
-
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 = arr[:2]
 arr2 = arr[-5:]
