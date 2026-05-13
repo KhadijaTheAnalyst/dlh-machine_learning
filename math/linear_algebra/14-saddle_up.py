f4e0c4f27d6e6c66de93cb963fbe08625e442e8d
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-13 13:21:53 +0200

    dot prodcut of matrices

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This modules represents matrix multliplication"""
+
+
+def np_matmul(mat1, mat2):
+    """Multiply two matrices, rows into column"""
+    return np.matmul(mat1, mat2)  # return mat1 @ mat2
