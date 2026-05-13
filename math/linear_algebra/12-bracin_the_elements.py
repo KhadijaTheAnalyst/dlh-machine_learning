84bf82a5597ab6d2d047283eb3baf9de0872dd40
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-13 13:05:37 +0200

    perform mathematics operation on matrices

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Perform element-wise addition, subtraction, multiplication,
+and division on numpy arrays."""
+
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """Perform element-wise addition, subtraction,
+    multiplication, and division."""
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
