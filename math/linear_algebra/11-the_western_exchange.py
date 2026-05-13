e5663e96b10b1e0d074cb84f0e08ed3bff249bbc
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-13 12:09:53 +0200

    Transpose the matrix

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to transpose a numpy array.
 
+The module includes:
+- np_transpose: Transposes a numpy array
+
+Author: KMustafa
+Date: May 2026
+"""
 def np_transpose(matrix):
     """Transpose a numpy array."""
     return matrix.T
