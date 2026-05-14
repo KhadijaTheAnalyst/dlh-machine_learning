6bc9e3783f66d40764e13c8c2c4482da7cee1e73
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-14 23:14:39 +0200

    adding a matrix

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module fot adding two matrices using recursion"""
+
+
 def add_matrices(mat1, mat2):
     """THIS FUNCTION ADD TWO MATRICES OF SAME SHAPE"""
 
@@ -9,10 +11,10 @@ def add_matrices(mat1, mat2):
     result = []
 
     for i in range(len(mat1)):
-        if type(mat1[i]) == list:
+        if isinstance(mat1[i], list):
             add_it = add_matrices(mat1[i], mat2[i])
-            if add_it == None:
-                 return None
+            if add_it is None:
+                return None
             result.append(add_it)
         else:
             result.append(mat1[i] + mat2[i])
