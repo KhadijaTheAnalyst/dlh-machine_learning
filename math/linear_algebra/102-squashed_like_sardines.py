e5c8d97b22a37af14a225418cb9ff2a9b9438e2b
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-15 12:06:18 +0200

    concatinating a matrix

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the recursive function to concatenate nD matrices """
 
-def cat_any_axis_recursive(mat1, mat2, axis=0):
+def cat_matrices(mat1, mat2, axis=0):
     """Function to concatenate matrices """
     if axis == 0:
         return list(mat1) + list(mat2)
