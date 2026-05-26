962672f30d4a21d553737addf706882db97ec258
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-26 10:55:07 +0200

    Makes changes

--- pipeline/databases/17-store.sql
+++ pipeline/databases/17-store.sql
@@ -1,8 +1,3 @@
--- This SQL script creates a trigger that decreases the quantity of items in the inventory after an order
--- is placed. The trigger is set to execute before a new record is inserted into the 'orders' table.
--- It updates the 'items' table by subtracting the number of items ordered from the current quantity
--- available for the specific item.  
-
 DELIMITER $$
 
 CREATE TRIGGER decrease_quantity
@@ -11,7 +6,7 @@ FOR EACH ROW
 BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
-    WHERE id = NEW.item_id;
+    WHERE name = NEW.item_name;
 END$$
 
 DELIMITER ;
