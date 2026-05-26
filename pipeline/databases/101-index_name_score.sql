db60e8783d0aa7f7df6efb8f900810520ba39fbb
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-26 10:44:37 +0200

    Makes changes

--- pipeline/databases/101-index_name_score.sql
+++ pipeline/databases/101-index_name_score.sql
@@ -1,5 +1,2 @@
--- Works with MySQL 8.0.13+
--- Create a composite index on the first letter of the name column and the score column
--- This optimizes searches by first letter and score
-
-CREATE INDEX idx_name_first_score ON names((LEFT(name, 1)), score);
+-- Create a composite index on the first letter of name and score
+CREATE INDEX idx_name_first_score ON names(name(1), score);
