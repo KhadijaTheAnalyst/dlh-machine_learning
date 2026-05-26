67ec920d036aba5593c0af716ac43ec5eac9f07b
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-05-26 10:42:45 +0200

    Makes changes

--- pipeline/databases/100-index_my_names.sql
+++ pipeline/databases/100-index_my_names.sql
@@ -1,12 +1,2 @@
 -- Create an index on the first letter of the name column
--- This optimizes searches by first letter
--- Works with MySQL 8.0.13+
-
-CREATE INDEX idx_name_first ON names((LEFT(name, 1)));
-
-How it works:
-
--- CREATE INDEX idx_name_first - Creates index named idx_name_first
--- ON names - On the names table
--- ((LEFT(name, 1))) - On the first character of name column
--- LEFT(name, 1) - Gets the first letter
+CREATE INDEX idx_name_first ON names(name(1));
