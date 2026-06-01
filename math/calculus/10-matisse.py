f90c3b4945a7560803c4452a80d47662f2ae2522
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-06-01 22:51:17 +0200

    fixing pycodestyle error

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that computes the derivative of a polynomial represented as a list of coefficients.
+Module that computes the derivative of a polynomial represented
+as a list of coefficients.
 """
 
 
@@ -12,7 +13,8 @@ def poly_derivative(poly):
         poly (list): coefficients where index = power of x
 
     Returns:
-        list: derivative coefficients, or [0] if derivative is zero, or None if invalid input
+        list: derivative coefficients, or [0] if derivative is zero,
+        or None if invalid input
     """
 
     if not isinstance(poly, list) or len(poly) == 0:
