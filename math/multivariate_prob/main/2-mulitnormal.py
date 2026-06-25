4b6385fbd213e7ab3c1d96a8746647f4002ed29a
Khadija The Analyst <engr.khadija.hussain@gmail.com>
2026-06-25 18:19:06 +0200

    adding all files

---
+++ math/multivariate_prob/main/2-mulitnormal.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Class that represents a Multivariate Normal distribution"""
+import numpy as np
+
+
+class MultiNormal:
+    """Represents a Multivariate Normal distribution"""
+
+    def __init__(self, data):
+        """Constructor
+        Args:
+            data: numpy.ndarray of shape (d, n)
+        """
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        d, n = data.shape
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        self.mean = np.mean(data, axis=1, keepdims=True)
+        X_centered = data - self.mean
+        self.cov = (X_centered @ X_centered.T) / (n - 1)
+
+    def pdf(self, x):
+        """Calculates the PDF at a data point
+        Args:
+            x: numpy.ndarray of shape (d, 1)
+        Returns:
+            float: value of the PDF
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        diff = x - self.mean
+
+        coeff = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        exponent = -0.5 * (diff.T @ inv @ diff)[0, 0]
+
+        return float(coeff * np.exp(exponent))
+    
